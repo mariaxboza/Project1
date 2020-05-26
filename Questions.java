beb49d5cd1121e380bb145961dc081ab423d7773
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-26 18:37:02 -0400

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,4 +1,5 @@
 public class Questions {
+    // Class to create the questions able to ask
 
     String q1 = "Is the person a male?\n";
     String q2 = "Is the person a female?";
@@ -9,9 +10,12 @@ public class Questions {
     String q7 = "Does the person have orange hair?\n";
     String q8 = "Does the person have white hair?\n";
 
+    // Array of all the possible questions
     String[][] ques = {{q1, q2}, {q3, q4} , {q5, q6}, {q7, q8}};
 
+    // Getter method for the question
     public String[][] getQues() { return ques;}
 
+    // Constuctor that creates all the questions.
     public Questions(){}
 }
