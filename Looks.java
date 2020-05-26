83e8b1fe6eb23d1b1dd690b7b7256605d40767c1
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-26 18:31:02 -0400

    Update Looks.java

--- Looks.java
+++ Looks.java
@@ -1,3 +1,3 @@
-public enum Looks {
+public enum Looks { // This class is to make the different things that each people for example if it is either male of female.
     MALE, FEMALE, WHITE, BLACK, ORANGE, HAT, GLASSES, YELLOW
 }
